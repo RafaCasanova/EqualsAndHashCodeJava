9c0bade610d5cc9cb9cb0142aeec00a001d4874b
Rafael Casanova <rafa.casanova.matos@gmail.com>
2021-07-02 21:18:47 -0300

    Implementação e teste de objetos iguais atravez do equals and hashcode

--- src/Produto.java
+++ src/Produto.java
@@ -1,24 +1,25 @@
 import java.text.DecimalFormat;
+import java.util.Objects;
+
 public class Produto {
     private String tipo;
     private String nome;
     private Double preco;
-    //private String referencia;//A referencia e os 3 primeiros chars transformado em int de tipo e nome concatenados.
+    private String referencia;//A referencia e os 3 primeiros chars transformado em int de tipo e nome concatenados.
 
     public Produto(String tipo, String nome, Double preco) {
     this.tipo = tipo;
     this.nome= nome;
     this.preco = preco;
 
-//        String referencia1 = "";
-//        String referencia2 = "";
-//        for(int i=0;i<3;i++){
-//            int letra1 = nome.charAt(i);
-//            int letra2 = tipo.charAt(i);
-//            referencia1 += letra1;
-//            referencia2 += letra2;
-//        }
-//        this.referencia = (referencia1 + referencia2);
+        String referencia1 = "";
+
+        for(int i=0;i<nome.length();i++){
+            int letra1 = nome.charAt(i);
+            referencia1 += letra1;
+
+        }
+        this.referencia = (referencia1);
     }
 
 
@@ -38,8 +39,18 @@ public class Produto {
         return "R$" + df.format(this.preco);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return tipo.equals(produto.tipo) && nome.equals(produto.nome);
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(tipo, nome);
+    }
 //    public String getReferencia(){
 //        return this.referencia;
 //    }
